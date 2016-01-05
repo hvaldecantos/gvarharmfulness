c400abe73cf003f22eb47e2a1b3a429289f8770f
Héctor Valdecantos <hvaldecantos@gmail.com>
2016-01-04 19:08:18 -0500

    add a script to generate a csv file with all global variables definitions. The file containes the revision, file, and line number where the global variable was defined.

---
+++ get_gvar_def.rb
@@ -0,0 +1,34 @@
+require 'json'
+require 'csv'
+require 'set'
+
+# mongoexport --db i_cpio --collection globals | ruby get_gvar_def.rb 
+
+project = ARGV[0]
+
+contents = STDIN.read
+json = JSON.parse(contents)
+
+total_gv = 0
+
+headers = ["gv", "filename", "line", "sha", "bugcount"]
+csv_filename = "results/#{project}_gvar_def.csv"
+CSV.open(csv_filename, "w+") do |row|
+  row << headers
+end
+
+json.each do |k,v|
+
+  if not v["name"]
+   next
+  end
+ 
+  total_gv += 1
+
+  CSV.open(csv_filename, "a+") do |row|
+    row << [v["name"],v["filename"],v["line_num"],v["first_sha"],v["bug_count"]]
+  end
+  
+end
+
+puts "total gv: " + total_gv.to_s
